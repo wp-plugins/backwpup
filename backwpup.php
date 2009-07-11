5a39c0b96ab61e22d74ffe8d1a1e0f8cc00a6cbc
danielhuesken <danielhuesken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-11 15:22:41 +0000

    Changed readme
    git-svn-id: https://plugins.svn.wordpress.org/backwpup/trunk@134187 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- backwpup.php
+++ backwpup.php
@@ -28,17 +28,11 @@ Domain Path: /lang/
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 */
 
-/*
-Change log:
-  Version 0.5.0: 	Inital test Release
-
-*/
-
 
 //Set plugin dirname
 define('BACKWPUP_PLUGIN_DIR', dirname(plugin_basename(__FILE__)));
 //Set Plugin Version
-define('BACKWPUP_VERSION', '0.5.0');
+define('BACKWPUP_VERSION', '0.5.5');
 
 //Version check
 if (version_compare($wp_version, '2.8', '<')) { // Let only Activate on WordPress Version 2.8 or heiger
