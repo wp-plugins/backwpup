b45c918aa67f8e52e1653fcf17acb5519f8980ff
danielhuesken <danielhuesken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-29 07:41:07 +0000

    bump version 0.8.1 With pcl Zip logging
    git-svn-id: https://plugins.svn.wordpress.org/backwpup/tags/0.8.1@246899 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ app/options-settings.php
@@ -0,0 +1,122 @@
+<?PHP 
+// don't load directly 
+if ( !defined('ABSPATH') ) 
+	die('-1');
+?>
+<div class="wrap">
+	<div id="icon-tools" class="icon32"><br /></div>
+<h2><?php _e("BackWPup Settings", "backwpup"); ?></h2>
+<ul class="subsubsub"> 
+<li><a href="admin.php?page=BackWPup"><?PHP _e('Jobs','backwpup'); ?></a> |</li> 
+<li><a href="admin.php?page=BackWPup&amp;action=logs"><?PHP _e('Logs','backwpup'); ?></a> |</li>
+<li><a href="admin.php?page=BackWPup&amp;action=tools"><?PHP _e('Tools','backwpup'); ?></a> |</li>
+<li><a href="admin.php?page=BackWPup&amp;action=settings" class="current"><?PHP _e('Settings','backwpup'); ?></a></li>
+</ul>
+
+<form method="post" action="">
+<input type="hidden" name="action" value="savecfg" />
+<?php  wp_nonce_field('backwpup-cfg'); ?>
+
+<table class="form-table">
+
+
+<tr valign="top"> 
+<th scope="row"><label for="memory"><?PHP _e('Max Memory Usage','backwpup'); ?></label></th> 
+<td id="memory">
+<?php 
+echo __('PHP.ini Memory Limit:','backwpup').' '.ini_get('memory_limit').'<br />'; 
+	
+if (empty($cfg['memorylimit']))
+	$cfg['memorylimit']='128M';
+	
+if (!function_exists('memory_get_usage')) 
+	echo __('Set Memory limit:','backwpup').'<input name="memorylimit" type="text" value="'.$cfg['memorylimit'].'" class="small-text" />';
+ else 
+	echo '<span class="description">'.__('Memory will be automatically increased!!!','backwpup').'</span><input name="memorylimit" type="hidden" value="'.$cfg['memorylimit'].'"  />';
+
+?>
+</td> 
+</tr> 
+
+
+
+<tr valign="top">
+<th scope="row"><label for="mailtype"><?PHP _e('Mail Send:','backwpup'); ?></label></th> 
+<td id="mailtype">
+<?PHP _e('Sender Email:','backwpup'); ?><input name="mailsndemail" type="text" value="<?PHP echo $cfg['mailsndemail'];?>" class="user" /><br />
+<?PHP _e('Sender Name:','backwpup'); ?><input name="mailsndname" type="text" value="<?PHP echo $cfg['mailsndname'];?>" class="user" /><br />
+<?PHP _e('Send mail method:','backwpup'); 
+echo '<select name="mailmethod">';
+echo '<option value="mail"'.selected('mail',$cfg['mailmethod'],false).'>'.__('PHP: mail()','backwpup').'</option>';
+echo '<option value="Sendmail"'.selected('Sendmail',$cfg['mailmethod'],false).'>'.__('Sendmail','backwpup').'</option>';
+echo '<option value="SMTP"'.selected('SMTP',$cfg['mailmethod'],false).'>'.__('SMTP','backwpup').'</option>';
+echo '</select>';
+?><br />
+<?PHP _e('Sendmail Path:','backwpup'); ?><input name="mailhost" type="text" value="<?PHP echo $cfg['mailsendmail'];?>" class="regular-text" /><br />
+<?PHP _e('SMTP Hostname:','backwpup'); ?><input name="mailhost" type="text" value="<?PHP echo $cfg['mailhost'];?>" class="regular-text" /><br />
+<?PHP _e('SMTP Secure Connection:','backwpup');
+echo '<select name="mailsecure">';
+echo '<option value=""'.selected('',$cfg['mailsecure'],false).'>'.__('none','backwpup').'</option>';
+echo '<option value="ssl"'.selected('ssl',$cfg['mailsecure'],false).'>SSL</option>';
+echo '<option value="tls"'.selected('tls',$cfg['mailsecure'],false).'>TLS</option>';
+echo '</select>';
+?><br />
+<?PHP _e('SMTP Username:','backwpup'); ?><input name="mailuser" type="text" value="<?PHP echo $cfg['mailuser'];?>" class="user" /><br />
+<?PHP _e('SMTP Password:','backwpup'); ?><input name="mailpass" type="password" value="<?PHP echo base64_decode($cfg['mailpass']);?>" class="password" /><br />
+</td> 
+</tr>
+
+<tr valign="top">
+<th scope="row"><label for="maxlogs"><?PHP _e('Max. Number of Logs','backwpup'); ?></label></th> 
+<td>
+<input name="maxlogs" id="maxlogs" type="text" value="<?PHP echo $cfg['maxlogs'];?>" class="small-text" /><span class="description"><?PHP _e('0=off','backwpup');?> <?PHP _e('Oldest log will deleted first.','backwpup');?></span>
+</td> 
+</tr>
+
+<tr valign="top"> 
+<th scope="row"><label for="dirlogs"><?PHP _e('Log file Folder:','backwpup'); ?></label></th> 
+<td>
+<input name="dirlogs" type="text" value="<?PHP echo $cfg['dirlogs'];?>" class="regular-text" /><br />
+</td> 
+</tr> 
+
+<tr valign="top"> 
+<th scope="row"><label for="disablewpcron"><?PHP _e('Disable WP-Cron:','backwpup'); ?></label></th> 
+<td>
+<input class="checkbox" id="disablewpcron" type="checkbox"<?php checked($cfg['disablewpcron'],true,true);?> name="disablewpcron" value="1"/>
+ <?PHP _e('Use Cron job of Hoster and disable WP_Cron','backwpup'); ?><br />
+<?PHP _e('You must set up a cron job that calls:','backwpup'); ?><br />
+<i> php -q <?PHP echo ABSPATH.'wp-cron.php'; ?></i><br /> 
+<?PHP _e('or URL:','backwpup'); ?> <i><?PHP echo trailingslashit(get_option('siteurl')).'wp-cron.php'; ?></i><br /> 
+</td> 
+</tr> 
+
+<tr valign="top"> 
+<th scope="row"><label for="dirtemp"><?PHP _e('Temp Folder for Backups:','backwpup'); ?></label></th> 
+<td>
+<input name="dirtemp" type="text" value="<?PHP echo $cfg['dirtemp'];?>" class="regular-text" /><br />
+</td> 
+</tr> 
+
+<tr valign="top"> 
+<th scope="row"><label for="pcl_log_level"><?PHP _e('Log Level for PCL ZIP:','backwpup'); ?></label></th> 
+<td>
+<?PHP
+echo '<select name="pcl_log_level">';
+echo '<option value="1"'.selected(1,$cfg['pcl_log_level'],false).'>'.__('Level 1 (Errors)','backwpup').'</option>';
+echo '<option value="2"'.selected(2,$cfg['pcl_log_level'],false).'>'.__('Level 2 (Min.)','backwpup').'</option>';
+echo '<option value="3"'.selected(3,$cfg['pcl_log_level'],false).'>'.__('Level 3','backwpup').'</option>';
+echo '<option value="4"'.selected(4,$cfg['pcl_log_level'],false).'>'.__('Level 4','backwpup').'</option>';
+echo '<option value="5"'.selected(5,$cfg['pcl_log_level'],false).'>'.__('Level 5 (Max.)','backwpup').'</option>';
+echo '</select>';
+?><br />
+</td> 
+</tr> 
+
+</table>
+ 
+<p class="submit"> 
+<input type="submit" name="Submit" class="button-primary" value="<?php _e('Save Changes', 'backwpup'); ?>" /> 
+</p> 
+</form>
+</div>
---
+++ readme.txt
@@ -0,0 +1,128 @@
+=== BackWPup ===
+Contributors: danielhuesken
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=daniel%40huesken-net%2ede&item_name=Daniel%20Huesken%20Plugin%20Donation&item_number=BackWPup&no_shipping=0&no_note=1&tax=0&currency_code=EUR&lc=DE&bn=PP%2dDonationsBF&charset=UTF%2d8
+Tags: backup, admin, file, Database, mysql, cron, ftp, S3
+Requires at least: 2.8
+Tested up to: 3.0.0
+Stable tag: 0.8.1
+
+Backup and more of your WordPress Blog Database and Files
+
+== Description ==
+
+This Plugin is under heavy Development. Please test it and give feedback!!!.
+Plugin Requires PHP 5.
+
+Backup and more your Blog.
+
+* Database Backup
+* Optimize Database
+* Check\Repair Database
+* File Backup
+* Uses PCLZIP class of WordPress
+* Store backup to Folder
+* Store backup to FTP Server
+* Store backup to Amazon S3
+* Send Log/Backup by eMail
+
+
+I can give no WARRANTY to any backups...
+
+== Installation ==
+
+1. Download BackWPup Plugin.
+1. Decompress and upload the contents of the archive into /wp-content/plugins/.
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Where is the Database dump on DB+File backup =
+
+in the root folder of the zip Archive. <i>DBName</i>.sql
+
+
+== Screenshots ==
+
+1. Job Page
+
+== Changelog ==
+= 0.8.1 =
+* use global var instat of constant for log file
+* PCL Zip Trace included with setting for log Level
+
+= 0.8.0 =
+* Fiexed not working default setttings on settingspage
+* crate .htaccsses on Apache and index.html on other webserver
+* fixed global for $wp_version
+* set max execution time to 0 for unlimeted
+* use WP function to generate options tables
+* Backup file list and zip creation changes
+* Added support for Amazon S3
+* Only works with PHP 5 now
+* Cmplete rewrite of job doing as PHP5 class
+* PHP errors now in Backup log
+* Log now in files
+
+= 0.7.2 =
+* make FTP any more robust
+* increased memory for Zip Files
+* make date with date_i18n
+
+= 0.7.1 =
+* FTP Conection test changes
+* no Errors in Log for FTP ALLO command.
+
+= 0.7.0 =
+* set ftp Connection timeout to 10 sec
+* fix bug for DB tables exclude
+* DB Backup in mySQL Client encoding now
+* Fixed missing ; in DB Backup
+* Added tool DB Restore with automatic Blog Url/Path change
+
+= 0.6.5 =
+* Prevent direct file loading
+* job working in iframe
+* colored logs
+* HTML fixes
+* spell check
+
+= 0.6.4 =
+* New option to delete old logs
+* Backup file deletion separated form logs deletion
+* make dashboard widget smaller
+* added massages
+* bug fixes
+
+= 0.6.3 =
+* use ftp_row for login and other commands
+* Add option to send only email on errors
+* Internal structure changes
+* Add option to disable WP-Cron and use Hoster cron
+* bug fixes
+
+= 0.6.2 =
+* Added setting for memory_limit if needed
+* Added setting for Max. Script execution time
+* Added job option to make Max file size for sending via mail
+* bug fixes and little improvements
+
+= 0.6.1 =
+* Added setting for Send Mail type.
+* Optimize Memory usage again
+* Fixed Bug that cron not work
+
+= 0.6.0 =
+* Add Dashboard Widget
+* Add Database Check
+* Add Backup file transfer to FTP Server
+* Save log files in own database table
+* Optimize Memory usage
+* Optimize File system access
+* DB dump with own function
+* fixed some Bugs
+
+= 0.5.5 =
+* removed log files. Log now stored in Database
+
+= 0.5.0 =
+* Initial release
