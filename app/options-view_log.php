03e573a659d443230f90c0c74744e0769aa404a9
danielhuesken <danielhuesken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-27 08:49:25 +0000

    fixed cache problem on runnow
    git-svn-id: https://plugins.svn.wordpress.org/backwpup/tags/1.0.3@257751 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ app/options-view_log.php
@@ -0,0 +1,21 @@
+<?PHP 
+// don't load directly 
+if ( !defined('ABSPATH') ) 
+	die('-1');
+?>
+<div class="wrap">
+	<div id="icon-tools" class="icon32"><br /></div>
+<h2><?php _e("BackWPup View Log", "backwpup"); ?></h2>
+<ul class="subsubsub"> 
+<li><a href="admin.php?page=BackWPup"><?PHP _e('Jobs','backwpup'); ?></a> |</li> 
+<li><a href="admin.php?page=BackWPup&amp;action=logs"><?PHP _e('Logs','backwpup'); ?></a> |</li>
+<li><a href="admin.php?page=BackWPup&amp;action=tools"><?PHP _e('Tools','backwpup'); ?></a> |</li>
+<li><a href="admin.php?page=BackWPup&amp;action=settings"><?PHP _e('Settings','backwpup'); ?></a></li>
+</ul>
+<br class="clear" /> 
+<big><?php
+_e('View Log','backwpup');
+echo ' <strong>'.basename($_GET['logfile']).'</strong>';
+?></big>
+<iframe src="<?PHP  echo wp_nonce_url(plugins_url('options-view_log-iframe.php',__FILE__).'?ABSPATH='.ABSPATH.'&logfile=' . $_GET['logfile'], 'viewlognow_'.basename($_GET['logfile'])); ?>" name="Logframe" id="Logframe" width="100%" height="450" align="left" scrolling="auto" style="border: 1px solid gray" frameborder="0"></iframe>
+</div>
