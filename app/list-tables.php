ccd5a109e3e783a621125574d6ea66a949fc8d65
danielhuesken <danielhuesken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-17 11:37:43 +0000

    = 1.3.6 = * long file list not longer displayed in logs. * Added option to see detailed file list * removed FTP Alloc command * set FTP normal mode if pasive mode disabled * remove FTP heler function and use FTP PHP functions * spend file list 2MB free memory
    git-svn-id: https://plugins.svn.wordpress.org/backwpup/tags/1.3.6@290614 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ app/list-tables.php
@@ -0,0 +1,486 @@
+<?PHP
+//backwarts copatibility lower than wp 3.1
+if (!class_exists('WP_List_Table')) {
+	if (is_file(trailingslashit(ABSPATH).'wp-admin/includes/list-table.php'))
+		include_once( trailingslashit(ABSPATH).'wp-admin/includes/list-table.php' );
+	else
+		include_once('compatibility/list-table.php');
+}
+
+class BackWPup_Jobs_Table extends WP_List_Table {
+	function BackWPup_Jobs_Table() {
+		global $current_screen;
+		parent::WP_List_Table( array(
+			'screen' => $current_screen,
+			'plural' => 'jobs',
+			'singular' => 'job'
+		) );
+	}
+	
+	function check_permissions() {
+		if ( !current_user_can( 10 ) )
+			wp_die( __( 'No rights' ) );
+	}	
+	
+	function prepare_items() {
+		global $mode;
+		$this->items=get_option('backwpup_jobs');
+		$mode = empty( $_REQUEST['mode'] ) ? 'list' : $_REQUEST['mode'];
+	}
+	
+	function pagination( $which ) {
+		global $mode;
+
+		parent::pagination( $which );
+
+		if ( 'top' == $which )
+			$this->view_switcher( $mode );
+	}
+	
+	function no_items() {
+		_e( 'No Jobs.','backwpup');
+	}
+
+	function get_bulk_actions() {
+		$actions = array();
+		$actions['delete'] = __( 'Delete' );
+
+		return $actions;
+	}
+	
+	function get_columns() {
+		$posts_columns = array();
+		$posts_columns['cb'] = '<input type="checkbox" />';
+		$posts_columns['id'] = __('ID','backwpup');
+		$posts_columns['jobname'] = __('Job Name','backwpup');
+		$posts_columns['type'] = __('Type','backwpup');
+		$posts_columns['info'] = __('Information','backwpup');
+		$posts_columns['next'] = __('Next Run','backwpup');
+		$posts_columns['last'] = __('Last Run','backwpup');
+		return $posts_columns;
+	}
+
+	function get_sortable_columns() {
+		return array();
+	}	
+
+	function get_hidden_columns() {
+		return (array) get_user_option( 'backwpup_jobs_columnshidden' );
+	}
+	
+	function display_rows() {
+		$style = '';
+		foreach ( $this->items as $jobid => $jobvalue ) {
+			$jobvalue=backwpup_check_job_vars($jobvalue,$jobid);//Set and check job settings
+			$style = ( ' class="alternate"' == $style ) ? '' : ' class="alternate"';
+			echo "\n\t", $this->single_row( $jobid, $jobvalue, $style );
+		}
+	}
+	
+	function single_row( $jobid, $jobvalue, $style = '' ) {
+		global $mode;
+		list( $columns, $hidden ) = $this->get_column_headers();
+		$r = "<tr id='jodid-$jobid'$style>";
+		foreach ( $columns as $column_name => $column_display_name ) {
+			$class = "class=\"$column_name column-$column_name\"";
+
+			$style = '';
+			if ( in_array( $column_name, $hidden ) )
+				$style = ' style="display:none;"';
+
+			$attributes = "$class$style";
+			
+			switch( $column_name ) {
+				case 'cb':
+					$r .=  '<th scope="row" class="check-column"><input type="checkbox" name="jobs[]" value="'. esc_attr($jobid) .'" /></th>';
+					break;
+				case 'id':
+					$r .=  "<td $attributes>".$jobid."</td>"; 
+					break;
+				case 'jobname':
+					$r .=  "<td $attributes><strong><a href=\"".wp_nonce_url('admin.php?page=BackWPup&subpage=edit&jobid='.$jobid, 'edit-job')."\" title=\"".__('Edit:','backwpup').$jobvalue['name']."\">".esc_html($jobvalue['name'])."</a></strong>";
+					$actions = array();
+					if (empty($jobvalue['logfile']) and empty($jobvalue['starttime'])) {
+						$actions['edit'] = "<a href=\"" . wp_nonce_url('admin.php?page=BackWPup&subpage=edit&jobid='.$jobid, 'edit-job') . "\">" . __('Edit') . "</a>";
+						$actions['copy'] = "<a href=\"" . wp_nonce_url('admin.php?page=BackWPup&action=copy&jobid='.$jobid, 'copy-job_'.$jobid) . "\">" . __('Copy','backwpup') . "</a>";
+						$actions['delete'] = "<a class=\"submitdelete\" href=\"" . wp_nonce_url('admin.php?page=BackWPup&action=delete&jobs[]='.$jobid, 'bulk-jobs') . "\" onclick=\"if ( confirm('" . esc_js(__("You are about to delete this Job. \n  'Cancel' to stop, 'OK' to delete.","backwpup")) . "') ) { return true;}return false;\">" . __('Delete') . "</a>";
+						$actions['runnow'] = "<a href=\"" . wp_nonce_url('admin.php?page=BackWPup&subpage=runnow&jobid='.$jobid, 'runnow-job_'.$jobid) . "\">" . __('Run Now','backwpup') . "</a>";
+					} else {
+						$actions['clear'] = "<a class=\"submitdelete\" href=\"" . wp_nonce_url('admin.php?page=BackWPup&action=clear&jobid='.$jobid, 'clear-job_'.$jobid) . "\">" . __('Clear','backwpup') . "</a>";
+					}
+					$action_count = count($actions);
+					$i = 0;
+					$r .=  '<br /><div class="row-actions">';
+					foreach ( $actions as $action => $linkaction ) {
+						++$i;
+						( $i == $action_count ) ? $sep = '' : $sep = ' | ';
+						$r .=  "<span class='$action'>$linkaction$sep</span>";
+					}
+					$r .=  '</div>';
+					$r .=  '</td>';
+					break;	
+				case 'type':
+					$r .=  "<td $attributes>";
+					$r .=  backwpup_backup_types($jobvalue['type'],false);
+					$r .=  "</td>";
+					break;
+				case 'info':
+					$r .=  "<td $attributes>";
+					if (in_array('FILE',explode('+',$jobvalue['type']))) {
+						$files=backwpup_calc_file_size($jobvalue);
+						$r .=  __("Files Size:","backwpup")." ".backwpup_formatBytes($files['size'])."<br />";
+						if ( 'excerpt' == $mode ) {
+							$r .=  __("Files count:","backwpup")." ".$files['num']."<br />";
+						}
+					}
+					if (in_array('DB',explode('+',$jobvalue['type'])) or in_array('OPTIMIZE',explode('+',$jobvalue['type'])) or in_array('CHECK',explode('+',$jobvalue['type']))) {
+						$dbsize=backwpup_calc_db_size($jobvalue);
+						$r .=  "DB Size: ".backwpup_formatBytes($dbsize['size'])."<br />";
+						if ( 'excerpt' == $mode ) {
+							$r .=  __("DB Tables:","backwpup")." ".$dbsize['num']."<br />";
+							$r .=  __("DB Rows:","backwpup")." ".$dbsize['rows']."<br />";
+						}
+					}
+					$r .=  "</td>";
+					break;
+				case 'next':
+					$r .= "<td $attributes>";
+					if ($jobvalue['starttime']>0 and !empty($jobvalue['logfile'])) {
+						$runtime=current_time('timestamp')-$jobvalue['starttime'];
+						$r .=  __('Running since:','backwpup').' '.$runtime.' '.__('sec.','backwpup');
+					} elseif ($jobvalue['activated']) {
+						$r .=  date(get_option('date_format'),$jobvalue['cronnextrun']).'<br />'. date(get_option('time_format'),$jobvalue['cronnextrun']);
+					} else {
+						$r .= __('Inactive','backwpup');
+					}
+					if ( 'excerpt' == $mode ) {
+						$r .= '<br />'.__('<a href="http://wikipedia.org/wiki/Cron" target="_blank">Cron</a>:','backwpup').' '.$jobvalue['cron'];
+					}
+					$r .=  "</td>";
+					break;
+				case 'last':
+					$r .=  "<td $attributes>";
+					if ($jobvalue['lastrun']) {
+						$r .=  date_i18n(get_option('date_format'),$jobvalue['lastrun']).'<br />'. date_i18n(get_option('time_format'),$jobvalue['lastrun']); 
+						if (isset($jobvalue['lastruntime']))
+							$r .=  '<br />'.__('Runtime:','backwpup').' '.$jobvalue['lastruntime'].' '.__('sec.','backwpup');
+					} else {
+						$r .= __('None','backwpup');
+					}
+					$r .=  "</td>";
+					break;
+			}
+		}
+		$r .= '</tr>';
+		return $r;
+	}
+}
+
+class BackWPup_Logs_Table extends WP_List_Table {
+	function BackWPup_Logs_Table() {
+		global $current_screen;
+		parent::WP_List_Table( array(
+			'screen' => $current_screen,
+			'plural' => 'logs',
+			'singular' => 'log'
+		) );
+	}
+	
+	function check_permissions() {
+		if ( !current_user_can( 10 ) )
+			wp_die( __( 'No rights' ) );
+	}	
+	
+	function prepare_items() {	
+		
+		$per_page = (int) get_user_option( 'backwpup_logs_per_page' );
+		if ( empty( $per_page ) || $per_page < 1 )
+			$per_page = 20;	
+		
+		//load logs
+		$cfg=get_option('backwpup');
+		$logfiles=array();
+		if ( $dir = @opendir( $cfg['dirlogs'] ) ) {
+			while (($file = readdir( $dir ) ) !== false ) {
+				if (is_file($cfg['dirlogs'].'/'.$file) and 'backwpup_log_' == substr($file,0,strlen('backwpup_log_')) and  '.html' == substr($file,-5)) 
+					$logfiles[]=$file;
+			}
+			closedir( $dir );
+			rsort($logfiles);
+		}
+		//by page
+		$start=intval( ( $this->get_pagenum() - 1 ) * $per_page );
+		$end=$start+$per_page;
+		if ($end>count($logfiles))
+			$end=count($logfiles);
+		
+		for ($i=$start;$i<$end;$i++) {
+			$this->items[] = $logfiles[$i];
+		}
+		
+		$this->set_pagination_args( array(
+			'total_items' => count($logfiles),
+			'per_page' => $per_page
+		) );
+
+	}
+	
+	function no_items() {
+		_e( 'No Logs.','backwpup');
+	}
+	
+	function get_bulk_actions() {
+		$actions = array();
+		$actions['delete'] = __( 'Delete' );
+
+		return $actions;
+	}
+	
+	function get_columns() {
+		$posts_columns = array();
+		$posts_columns['cb'] = '<input type="checkbox" />';
+		$posts_columns['id'] = __('Job','backwpup');
+		$posts_columns['type'] = __('Type','backwpup');
+		$posts_columns['log'] = __('Backup/Log Date/Time','backwpup');
+		$posts_columns['status'] = __('Status','backwpup');
+		$posts_columns['size'] = __('Size','backwpup');
+		$posts_columns['runtime'] = __('Runtime','backwpup');
+		return $posts_columns;
+	}
+
+	function get_sortable_columns() {
+		return array();
+	}	
+
+	function get_hidden_columns() {
+		return (array) get_user_option( 'backwpup_logs_columnshidden' );
+	}
+	
+	function display_rows() {
+		$style = '';
+		$cfg=get_option('backwpup');
+		foreach ( $this->items as $logfile ) {
+			$style = ( ' class="alternate"' == $style ) ? '' : ' class="alternate"';
+			$logdata=backwpup_read_logheader($cfg['dirlogs'].$logfile);
+			echo "\n\t", $this->single_row( $cfg['dirlogs'].$logfile, $logdata, $style );
+		}
+	}
+	
+	function single_row( $logfile, $logdata, $style = '' ) {
+		list( $columns, $hidden ) = $this->get_column_headers();
+		$r = "<tr id='".basename($logfile)."'$style>";
+		foreach ( $columns as $column_name => $column_display_name ) {
+			$class = "class=\"$column_name column-$column_name\"";
+
+			$style = '';
+			if ( in_array( $column_name, $hidden ) )
+				$style = ' style="display:none;"';
+
+			$attributes = "$class$style";
+			
+			switch($column_name) {
+				case 'cb':
+					$r .= '<th scope="row" class="check-column"><input type="checkbox" name="logfiles[]" value="'. esc_attr(basename($logfile)) .'" /></th>';
+					break;
+				case 'id':
+					$r .= "<td $attributes>".$logdata['jobid']."</td>"; 
+					break;
+				case 'type':
+					$r .= "<td $attributes>";
+					$r .= backwpup_backup_types($logdata['type'],false);
+					$r .= "</td>"; 
+					break;
+				case 'log':
+					$name='';
+					if (is_file($logvalue['backupfile']))
+						$name=basename($logvalue['backupfile']);
+				
+					$r .= "<td $attributes><strong><a href=\"".wp_nonce_url('admin.php?page=BackWPup&subpage=view_log&logfile='.$logfile, 'view-log_'.basename($logfile))."\" title=\"".__('View log','backwpup')."\">".date_i18n(get_option('date_format'),$logdata['logtime'])." ".date_i18n(get_option('time_format'),$logdata['logtime']).": <i>".$logdata['name']."</i></a></strong>";
+					$actions = array();
+					$actions['view'] = "<a href=\"" . wp_nonce_url('admin.php?page=BackWPup&subpage=view_log&logfile='.$logfile, 'view-log_'.basename($logfile)) . "\">" . __('View','backwpup') . "</a>";
+					$actions['delete'] = "<a class=\"submitdelete\" href=\"" . wp_nonce_url('admin.php?page=BackWPup&subpage=logs&action=delete&paged='.$this->get_pagenum().'&logfiles[]='.basename($logfile), 'bulk-logs') . "\" onclick=\"return showNotice.warn();\">" . __('Delete') . "</a>";
+					$actions['download'] = "<a href=\"" . wp_nonce_url('admin.php?page=BackWPup&subpage=logs&action=download&file='.$logfile, 'download-backup_'.basename($logfile)) . "\">" . __('Download','backwpup') . "</a>";
+					$action_count = count($actions);
+					$i = 0;
+					$r .= '<br /><div class="row-actions">';
+					foreach ( $actions as $action => $linkaction ) {
+						++$i;
+						( $i == $action_count ) ? $sep = '' : $sep = ' | ';
+						$r .= "<span class='$action'>$linkaction$sep</span>";
+					}
+					$r .= '</div>';
+					$r .= "</td>";
+					break;
+				case 'status':
+					$r .= "<td $attributes>";
+					if($logdata['errors']>0 or $logdata['warnings']>0) { 
+						if ($logdata['errors']>0)
+							$r .= '<span style="color:red;">'.$logdata['errors'].' '.__('ERROR(S)','backwpup').'</span><br />'; 
+						if ($logdata['warnings']>0)
+							$r .= '<span style="color:yellow;">'.$logdata['warnings'].' '.__('WARNING(S)','backwpup').'</span>'; 
+					} else { 
+						$r .= '<span style="color:green;">'.__('OK','backwpup').'</span>';
+					} 
+					$r .= "</td>"; 
+					break;
+				case 'size':
+					$r .= "<td $attributes>";
+					if (!empty($logdata['backupfilesize'])) {
+						$r .= backwpup_formatBytes($logdata['backupfilesize']);
+					} else {
+						$r .= __('only Log','backwpup');
+					}
+					$r .= "</td>"; 
+					break;
+				case 'runtime':
+					$r .= "<td $attributes>";
+					$r .= $logdata['runtime'].' '.__('sec.','backwpup');
+					$r .= "</td>"; 
+					break;					
+			}
+		}
+		$r .= '</tr>';
+		return $r;
+	}
+}
+
+class BackWPup_Backups_Table extends WP_List_Table {
+	function BackWPup_Backups_Table() {
+		global $current_screen;
+		parent::WP_List_Table( array(
+			'screen' => $current_screen,
+			'plural' => 'backups',
+			'singular' => 'backup'
+		) );
+	}
+	
+	function check_permissions() {
+		if ( !current_user_can( 10 ) )
+			wp_die( __( 'No rights' ) );
+	}	
+	
+	function prepare_items() {	
+		
+		$per_page = (int) get_user_option( 'backwpup_backups_per_page' );
+		if ( empty( $per_page ) || $per_page < 1 )
+			$per_page = 20;	
+		
+		//load logs
+		if (isset($_GET['paged'])) {
+			$backups=get_option('backwpup_backups_chache');
+		} else {
+			//Get Backup files
+			$backups=backwpup_get_backup_files();
+			update_option('backwpup_backups_chache',$backups);
+		}
+		
+		//by page
+		$start=intval( ( $this->get_pagenum() - 1 ) * $per_page );
+		$end=$start+$per_page;
+		if ($end>count($backups))
+			$end=count($backups);
+		
+		for ($i=$start;$i<$end;$i++) {
+			$this->items[] = $backups[$i];
+		}
+		
+		$this->set_pagination_args( array(
+			'total_items' => count($backups),
+			'per_page' => $per_page	
+		) );
+
+	}
+	
+	function no_items() {
+		_e( 'No Backups.','backwpup');
+	}
+	
+	
+	function get_bulk_actions() {
+		$actions = array();
+		$actions['delete'] = __( 'Delete' );
+
+		return $actions;
+	}
+	
+	function get_columns() {
+		$posts_columns = array();
+		$posts_columns['cb'] = '<input type="checkbox" />';
+		$posts_columns['backup'] = __('Backupfile','backwpup');
+		$posts_columns['size'] = __('Size','backwpup');
+		return $posts_columns;
+	}
+
+	function get_sortable_columns() {
+		return array();
+	}	
+
+	function get_hidden_columns() {
+		return (array) get_user_option( 'backwpup_backups_columnshidden' );
+	}
+	
+	function display_rows() {
+		$style = '';
+		$jobs=get_option('backwpup_jobs'); //Load jobs
+		foreach ( $this->items as $backup ) {
+			$style = ( ' class="alternate"' == $style ) ? '' : ' class="alternate"';
+			echo "\n\t", $this->single_row( $backup, backwpup_check_job_vars($jobs[$backup['jobid']],$backup['jobid']), $style );
+		}
+	}
+	
+	function single_row( $backup, $jobvalue, $style = '' ) {
+		list( $columns, $hidden ) = $this->get_column_headers();
+		$r = "<tr id='$logfile'$style>";
+		foreach ( $columns as $column_name => $column_display_name ) {
+			$class = "class=\"$column_name column-$column_name\"";
+
+			$style = '';
+			if ( in_array( $column_name, $hidden ) )
+				$style = ' style="display:none;"';
+
+			$attributes = "$class$style";
+			
+			switch($column_name) {
+				case 'cb':
+					$r .= '<th scope="row" class="check-column"><input type="checkbox" name="backupfiles[]" value="'. esc_attr($backup['file'].':'.$backup['jobid'].':'.$backup['type']) .'" /></th>';
+					break;
+				case 'backup':
+					if ($backup['type']=='FOLDER') {
+						$r .= "<td $attributes><strong>".basename($backup['file'])."</strong><br />".dirname($backup['file'])."/";
+					} elseif ($backup['type']=='S3') {
+						$r .= "<td $attributes><strong>".basename($backup['file'])."</strong><br />S3://".$jobvalue['awsBucket']."/".dirname($backup['file'])."/";
+					} elseif ($backup['type']=='FTP') {
+						$r .= "<td $attributes><strong>".basename($backup['file'])."</strong><br />ftp://".$jobvalue['ftphost'].dirname($backup['file'])."/";
+					} elseif ($backup['type']=='RSC') {
+						$r .= "<td $attributes><strong>".basename($backup['file'])."</strong><br />RSC://".$jobvalue['rscContainer']."/".dirname($backup['file'])."/";
+					} 
+					$actions = array();
+					$actions['delete'] = "<a class=\"submitdelete\" href=\"" . wp_nonce_url('admin.php?page=BackWPup&subpage=backups&action=delete&paged='.$this->get_pagenum().'&backupfiles[]='.esc_attr($backup['file'].':'.$backup['jobid'].':'.$backup['type']), 'bulk-backups') . "\" onclick=\"if ( confirm('" . esc_js(__("You are about to delete this Backup Archive. \n  'Cancel' to stop, 'OK' to delete.","backwpup")) . "') ) { return true;}return false;\">" . __('Delete') . "</a>";
+					$actions['download'] = "<a href=\"" . $backup['downloadurl'] . "\">" . __('Download','backwpup') . "</a>";
+					$action_count = count($actions);
+					$i = 0;
+					$r .= '<br /><div class="row-actions">';
+					foreach ( $actions as $action => $linkaction ) {
+						++$i;
+						( $i == $action_count ) ? $sep = '' : $sep = ' | ';
+						$r .= "<span class='$action'>$linkaction$sep</span>";
+					}
+					$r .= '</div>';
+					$r .= "</td>";
+					break;
+				case 'size':
+					$r .= "<td $attributes>";
+					if (!empty($backup['filesize']) and $backup['filesize']!=-1) {
+						$r .= backwpup_formatBytes($backup['filesize']);
+					} else {
+						$r .= __('?','backwpup');
+					}
+					$r .= "</td>";
+					break;
+			}
+		}
+		$r .= '</tr>';
+		return $r;
+	}
+}
+?>
---
+++ backwpup.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Plugin Name: BackWPup
+Plugin URI: http://danielhuesken.de/portfolio/backwpup/
+Description: Backup and more of your WordPress Blog Database and Files.
+Author: Daniel H&uuml;sken
+Version: 1.3.6
+Author URI: http://danielhuesken.de
+Text Domain: backwpup
+Domain Path: /lang/
+*/
+
+/*
+	Copyright 2010  Daniel Hüsken  (email : daniel@huesken-net.de)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// don't load directly
+if ( !defined('ABSPATH') )
+	die('-1');
+
+//Set plugin dirname
+define('BACKWPUP_PLUGIN_BASEDIR', dirname(plugin_basename(__FILE__)));
+//Set Plugin Version
+define('BACKWPUP_VERSION', '1.3.6');
+//load Text Domain
+load_plugin_textdomain('backwpup', false, BACKWPUP_PLUGIN_BASEDIR.'/lang');
+//Load functions file
+require_once(dirname(__FILE__).'/app/functions.php');
+//Plugin activate
+register_activation_hook(__FILE__, 'backwpup_plugin_activate');
+//Plugin deactivate
+register_deactivation_hook(__FILE__, 'backwpup_plugin_deactivate');
+//Plugin init
+add_action('plugins_loaded', 'backwpup_plugins_loaded');
+//Admin message
+add_action('admin_notices', 'backwpup_admin_notice'); 
+?>
