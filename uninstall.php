f8fd88f7184ae06ba831db6203e1987f686301d6
danielhuesken <danielhuesken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-04 18:13:37 +0000

    = 1.5.5 = * Updatet AWS SDK to ver.1.2.6 for Amazon S3 * Added AWS Regin "Northeast" (Japan) * Added Microsoft Azure (Blob) as backup destination * bug fixes
    git-svn-id: https://plugins.svn.wordpress.org/backwpup/tags/1.5.5@355335 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,8 @@
+<?PHP
+if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
+    exit();
+
+delete_option('backwpup');
+delete_option('backwpup_jobs');
+delete_option('backwpup_backups_chache');
+?>
