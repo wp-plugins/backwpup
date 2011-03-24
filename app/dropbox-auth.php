dfa46e57384120712ed35b56f38e360a0c2cd221
danielhuesken <danielhuesken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-24 09:48:23 +0000

    * dropbox improvments and bug fixes
    git-svn-id: https://plugins.svn.wordpress.org/backwpup/tags/1.6.2@364128 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ app/dropbox-auth.php
@@ -0,0 +1,25 @@
+<?PHP
+if (file_exists(trim($_GET['wpabs']).'wp-load.php')) {
+	require_once(trim($_GET['wpabs']).'wp-load.php'); /** Setup WordPress environment */
+} else {
+	header("HTTP/1.0 404 Not Found");
+}
+require_once (dirname(__FILE__).'/libs/dropbox/dropbox.php');
+$dropbox = new Dropbox(BACKWPUP_DROPBOX_APP_KEY, BACKWPUP_DROPBOX_APP_SECRET);
+//for Dropbox oAuth backlink
+if ($_GET['uid']>1 and !empty($_GET['oauth_token'])) {
+	$reqtoken=get_option('backwpup_dropboxrequest');
+	if ($reqtoken['oAuthRequestToken']==$_GET['oauth_token']) {
+		//Get Access Tokens
+		$oAuthStuff = $dropbox->oAuthAccessToken($reqtoken['oAuthRequestToken'],$reqtoken['oAuthRequestTokenSecret']);
+		//Save Tokens
+		$jobs=get_option('backwpup_jobs');
+		$jobs[$reqtoken['jobid']]['dropetoken']=$oAuthStuff['oauth_token'];
+		$jobs[$reqtoken['jobid']]['dropesecret']=$oAuthStuff['oauth_token_secret'];
+		update_option('backwpup_jobs',$jobs);
+		delete_option('backwpup_dropboxrequest');
+		//Go back to jobs page
+		header("Location: ".$reqtoken['referer']."#dropbox");
+	}
+} 
+?>
