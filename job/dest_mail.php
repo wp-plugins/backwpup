3a69cd3017c6add1927964f265a7ebbda949a9d8
danielhuesken <danielhuesken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-07 22:25:34 +0000

    tag 2.0.2
    git-svn-id: https://plugins.svn.wordpress.org/backwpup/tags/2.0.2@406508 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ job/dest_mail.php
@@ -0,0 +1,74 @@
+<?PHP
+if (!defined('BACKWPUP_JOBRUN_FOLDER')) {
+	header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+	header("Status: 404 Not Found");
+	die();
+}
+
+function dest_mail() {
+	if (empty($_SESSION['JOB']['mailaddress'])) {
+		$_SESSION['WORKING']['STEPSDONE'][]='DEST_MAIL'; //set done
+		return;
+	}
+	$_SESSION['WORKING']['STEPTODO']=filesize($_SESSION['JOB']['backupdir'].$_SESSION['STATIC']['backupfile']);
+	$_SESSION['WORKING']['STEPDONE']=0;
+	trigger_error($_SESSION['WORKING']['DEST_MAIL']['STEP_TRY'].'. '.__('Try to sending backup file with mail...','backwpup'),E_USER_NOTICE);
+
+	//Create PHP Mailer
+	require_once(realpath($_SESSION['WP']['ABSPATH'].$_SESSION['WP']['WPINC']).'/class-phpmailer.php');
+	$phpmailer = new PHPMailer();
+	//Setting den methode
+	if ($_SESSION['CFG']['mailmethod']=="SMTP") {
+		require_once(realpath($_SESSION['WP']['ABSPATH'].$_SESSION['WP']['WPINC']).'/class-smtp.php');
+		$phpmailer->Host=$_SESSION['CFG']['mailhost'];
+		$phpmailer->Port=$_SESSION['CFG']['mailhostport'];
+		$phpmailer->SMTPSecure=$_SESSION['CFG']['mailsecure'];
+		$phpmailer->Username=$_SESSION['CFG']['mailuser'];
+		$phpmailer->Password=base64_decode($_SESSION['CFG']['mailpass']);
+		if (!empty($_SESSION['CFG']['mailuser']) and !empty($_SESSION['CFG']['mailpass']))
+			$phpmailer->SMTPAuth=true;
+		$phpmailer->IsSMTP();
+		trigger_error(__('Send mail with SMTP','backwpup'),E_USER_NOTICE);
+	} elseif ($_SESSION['CFG']['mailmethod']=="Sendmail") {
+		$phpmailer->Sendmail=$_SESSION['CFG']['mailsendmail'];
+		$phpmailer->IsSendmail();
+		trigger_error(__('Send mail with Sendmail','backwpup'),E_USER_NOTICE);
+	} else {
+		$phpmailer->IsMail();
+		trigger_error(__('Send mail with PHP mail','backwpup'),E_USER_NOTICE);
+	}
+
+	trigger_error(__('Creating mail','backwpup'),E_USER_NOTICE);
+	$phpmailer->From     = $_SESSION['CFG']['mailsndemail'];
+	$phpmailer->FromName = $_SESSION['CFG']['mailsndname'];
+	$phpmailer->AddAddress($_SESSION['JOB']['mailaddress']);
+	$phpmailer->Subject  =  __('BackWPup File from','backwpup').' '.date('Y-m-d H:i',$_SESSION['JOB']['starttime']).': '.$_SESSION['JOB']['name'];
+	$phpmailer->IsHTML(false);
+	$phpmailer->Body  =  __('Backup File:','backwpup').$_SESSION['STATIC']['backupfile'];
+
+	//check file Size
+	if (!empty($_SESSION['JOB']['mailefilesize'])) {
+		$maxfilezise=abs($_SESSION['JOB']['mailefilesize']*1024*1024);
+		if (filesize($_SESSION['JOB']['backupdir'].$_SESSION['STATIC']['backupfile'])>$maxfilezise) {
+			trigger_error(__('Backup Archive too big for sending by mail','backwpup'),E_USER_ERROR);
+			$_SESSION['WORKING']['STEPDONE']=1;
+			$_SESSION['WORKING']['STEPSDONE'][]='DEST_MAIL'; //set done
+			return;
+		}
+	}
+
+	trigger_error(__('Adding Attachment to mail','backwpup'),E_USER_NOTICE);
+	need_free_memory(filesize($_SESSION['JOB']['backupdir'].$_SESSION['STATIC']['backupfile'])*5);
+	$phpmailer->AddAttachment($_SESSION['JOB']['backupdir'].$_SESSION['STATIC']['backupfile']);
+
+	trigger_error(__('Send mail....','backwpup'),E_USER_NOTICE);
+	@set_time_limit($_SESSION['CFG']['jobscriptruntimelong']);
+	if (false == $phpmailer->Send()) {
+		trigger_error(__('Can not send mail:','backwpup').' '.$phpmailer->ErrorInfo,E_USER_ERROR);
+	} else {
+		$_SESSION['WORKING']['STEPTODO']=filesize($_SESSION['JOB']['backupdir'].$_SESSION['STATIC']['backupfile']);
+		trigger_error(__('Mail send!!!','backwpup'),E_USER_NOTICE);
+	}
+	$_SESSION['WORKING']['STEPSDONE'][]='DEST_MAIL'; //set done
+}
+?>
---
+++ job/dest_s3.php
@@ -0,0 +1,77 @@
+<?PHP
+if (!defined('BACKWPUP_JOBRUN_FOLDER')) {
+	header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+	header("Status: 404 Not Found");
+	die();
+}
+
+function dest_s3() {
+	if (empty($_SESSION['JOB']['awsAccessKey']) or empty($_SESSION['JOB']['awsSecretKey']) or empty($_SESSION['JOB']['awsBucket'])) {
+		$_SESSION['WORKING']['STEPSDONE'][]='DEST_S3'; //set done	
+		return;
+	}
+	trigger_error($_SESSION['WORKING']['DEST_S3']['STEP_TRY'].'. '.__('Try to sending backup file to Amazon S3...','backwpup'),E_USER_NOTICE);
+	$_SESSION['WORKING']['STEPTODO']=2+filesize($_SESSION['JOB']['backupdir'].$_SESSION['STATIC']['backupfile']);
+	$_SESSION['WORKING']['STEPDONE']=0;
+
+	require_once(dirname(__FILE__).'/../libs/aws/sdk.class.php');
+	need_free_memory(26214400*1.1); 
+	
+	try {
+		$s3 = new AmazonS3($_SESSION['JOB']['awsAccessKey'], $_SESSION['JOB']['awsSecretKey']);
+		if ($s3->if_bucket_exists($_SESSION['JOB']['awsBucket'])) {
+			trigger_error(__('Connected to S3 Bucket:','backwpup').' '.$_SESSION['JOB']['awsBucket'],E_USER_NOTICE);
+			//Transfer Backup to S3
+			if ($_SESSION['JOB']['awsrrs']) //set reduced redundancy or not
+				$storage=AmazonS3::STORAGE_REDUCED;
+			else 
+				$storage=AmazonS3::STORAGE_STANDARD;
+			//set surl Prozess bar
+			$curlops=array();
+			if (function_exists('curl_progresscallback') and is_numeric(CURLOPT_PROGRESSFUNCTION))
+				$curlops=array(CURLOPT_NOPROGRESS=>false,CURLOPT_PROGRESSFUNCTION=>'curl_progresscallback',CURLOPT_BUFFERSIZE=>256);
+			else 
+				@set_time_limit($_SESSION['CFG']['jobscriptruntimelong']);
+			trigger_error(__('Upload to Amazon S3 now started ... ','backwpup'),E_USER_NOTICE);	
+			if ($s3->create_mpu_object($_SESSION['JOB']['awsBucket'], $_SESSION['JOB']['awsdir'].$_SESSION['STATIC']['backupfile'], array('fileUpload' => $_SESSION['JOB']['backupdir'].$_SESSION['STATIC']['backupfile'],'acl' => AmazonS3::ACL_PRIVATE,'storage' => $storage,'partSize'=>26214400,'curlopts'=>$curlops)))  {//transfere file to S3
+				$_SESSION['WORKING']['STEPTODO']=1+filesize($_SESSION['JOB']['backupdir'].$_SESSION['STATIC']['backupfile']);
+				trigger_error(__('Backup File transferred to S3://','backwpup').$_SESSION['JOB']['awsBucket'].'/'.$_SESSION['JOB']['awsdir'].$_SESSION['STATIC']['backupfile'],E_USER_NOTICE);
+				$_SESSION['JOB']['lastbackupdownloadurl']=$_SESSION['WP']['ADMINURL'].'?page=backwpupbackups&action=downloads3&file='.$_SESSION['JOB']['awsdir'].$_SESSION['STATIC']['backupfile'].'&jobid='.$_SESSION['JOB']['jobid'];
+			} else {
+				trigger_error(__('Can not transfer backup to S3.','backwpup'),E_USER_ERROR);
+			}
+			
+			if ($_SESSION['JOB']['awsmaxbackups']>0) { //Delete old backups
+				$backupfilelist=array();
+				if (($contents = $s3->list_objects($_SESSION['JOB']['awsBucket'],array('prefix'=>$_SESSION['JOB']['awsdir']))) !== false) {
+					foreach ($contents->body->Contents as $object) {
+						$file=basename($object->Key);
+						if ($_SESSION['JOB']['fileprefix'] == substr($file,0,strlen($_SESSION['JOB']['fileprefix'])) and $_SESSION['JOB']['fileformart'] == substr($file,-strlen($_SESSION['JOB']['fileformart'])))
+							$backupfilelist[]=$file;
+					}
+				}
+				if (sizeof($backupfilelist)>0) {
+					rsort($backupfilelist);
+					$numdeltefiles=0;
+					for ($i=$_SESSION['JOB']['awsmaxbackups'];$i<sizeof($backupfilelist);$i++) {
+						if ($s3->delete_object($_SESSION['JOB']['awsBucket'], $_SESSION['JOB']['awsdir'].$backupfilelist[$i])) //delte files on S3
+							$numdeltefiles++;
+						else
+							trigger_error(__('Can not delete file on S3://','backwpup').$_SESSION['JOB']['awsBucket'].'/'.$_SESSION['JOB']['awsdir'].$backupfilelist[$i],E_USER_ERROR);
+					}
+					if ($numdeltefiles>0)
+						trigger_error($numdeltefiles.' '.__('files deleted on S3 Bucket!','backwpup'),E_USER_NOTICE);
+				}
+			}					
+		} else {
+			trigger_error(__('S3 Bucket not exists:','backwpup').' '.$_SESSION['JOB']['awsBucket'],E_USER_ERROR);
+		}
+	} catch (Exception $e) {
+		trigger_error(__('Amazon S3 API:','backwpup').' '.$e->getMessage(),E_USER_ERROR);
+		return;
+	}
+	
+	$_SESSION['WORKING']['STEPDONE']++;
+	$_SESSION['WORKING']['STEPSDONE'][]='DEST_S3'; //set done
+}
+?>
