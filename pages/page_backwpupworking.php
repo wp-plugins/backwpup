4b861d41711ec5785c620b6941af94dc6e07cd9e
danielhuesken <danielhuesken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-22 20:13:37 +0000

    git-svn-id: https://plugins.svn.wordpress.org/backwpup/trunk@413908 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pages/page_backwpupworking.php
+++ pages/page_backwpupworking.php
@@ -46,3 +46,16 @@ if (!defined('ABSPATH')) {
 	}
 	?>
 </div>
+<div style="text-align:center;"><br />
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-0128840172400820";
+/* BackWPup Plugin horizontal */
+google_ad_slot = "5785357827";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+</div>
