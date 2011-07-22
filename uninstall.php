c8db75526e27d48ac8907236c0300fa26286988d
danielhuesken <danielhuesken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-22 16:42:52 +0000

    git-svn-id: https://plugins.svn.wordpress.org/backwpup/trunk@413816 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -9,18 +9,7 @@ global $wp_version;
 $blugurl=get_option('siteurl');
 if (defined('WP_SITEURL'))
 	$blugurl=WP_SITEURL;
-$ch=@curl_init();
-@curl_setopt($ch,CURLOPT_URL,'http://api.backwpup.com');
-@curl_setopt($ch,CURLOPT_POST,true);
-@curl_setopt($ch,CURLOPT_POSTFIELDS,array('URL'=>$blugurl,'EMAIL'=>get_option('admin_email'),'WP_VER'=>$wp_version,'BACKWPUP_VER'=>'0','ACTIVE'=>'D'));
-@curl_setopt($ch,CURLOPT_USERAGENT,'BackWPup');
-@curl_setopt($ch,CURLOPT_RETURNTRANSFER,false);
-@curl_setopt($ch,CURLOPT_FORBID_REUSE,true);
-@curl_setopt($ch,CURLOPT_FRESH_CONNECT,true);
-@curl_setopt($ch,CURLOPT_TIMEOUT,0.01);
-@curl_exec($ch);
-@curl_close($ch);
-
+wp_remote_post( 'http://api.backwpup.com', array('timeout' => 10, 'blocking' => false, 'sslverify' => false, 'body'=>array('URL'=>$blugurl,'EMAIL'=>get_option('admin_email'),'WP_VER'=>$wp_version,'BACKWPUP_VER'=>0,'ACTIVE'=>'D'), 'user-agent'=>'BackWPup'));
 delete_option('backwpup');
 delete_option('backwpup_jobs');
 delete_option('backwpup_last_activate');
