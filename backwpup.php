33aa23f4cf64b4874e094e55b8c4bc568dd55ab1
danielhuesken <danielhuesken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-23 18:31:13 +0000

    git-svn-id: https://plugins.svn.wordpress.org/backwpup/tags/2.1.0@414329 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- backwpup.php
+++ backwpup.php
@@ -41,7 +41,7 @@ define('BACKWPUP_PLUGIN_BASEURL',plugins_url('',__FILE__));
 //Set Plugin Version
 define('BACKWPUP_VERSION', '2.1.0');
 //Set Min Wordpress Version
-define('BACKWPUP_MIN_WORDPRESS_VERSION', '3.2');
+define('BACKWPUP_MIN_WORDPRESS_VERSION', '3.1');
 //Set User Capability
 define('BACKWPUP_USER_CAPABILITY', 'export');
 //Set useable destinations
